58dd0cd0ee7ad6002387e8b2922daed66003f30b
saiprasadvk <67091926+saiprasadvk@users.noreply.github.com>
2020-07-22 10:15:41 +0530

    Create python.py

---
+++ python.py
@@ -0,0 +1,12 @@
+1.Get all the numbers inbetween two numbers in the list
+    -Input should read from user (start & end values)
+    -Slicing methodalogy can use i/o - list = [1,2,3,4,5,5,6,10,20,40] Start - 5 End - 20 o/p [5,5,6,10,20]
+
+
+
+List = [1,2,3,4,5,5,6,10,20,40]
+
+Start = int(input("Enter the start value:"))
+End = int(input("Enter the end value:"))
+
+print(List[Start:End])
